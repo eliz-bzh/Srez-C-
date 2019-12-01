326b469925d18c066a228bbed308e4b67fd983ff
Liz <eliz.com>
2019-12-01 10:09:28 +0300

    Vector

---
+++ Srez/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Srez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Date time = new Date(23, 38, 72);
+            Console.WriteLine(time.f());
+            time.f2();
+            Console.WriteLine(time.ToString());
+            */
+            Complex start = new Complex(32, 16);
+            Console.WriteLine(start);
+            Console.WriteLine(start.f(2));
+            Console.WriteLine(start.abs());
+            Console.WriteLine(start.reverse());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Srez/Vector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Srez
+{
+    class Vector
+    {
+        private int x1;
+        public int X1 {
+            get
+            {
+                return x1;
+            }
+            set
+            {
+                x1 = value;
+            }
+        }
+        public int x2 { get; set; }
+        public int y1 { get; set; }
+        public int y2 { get; set; }
+
+        public Vector() { }
+        public Vector(int x1, int x2, int y1, int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+
+        ~Vector()
+        {
+            Console.WriteLine("Deleted");
+        }
+
+        public double length()
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+    }
+}
