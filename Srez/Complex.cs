374e629759b7f2ad3e23c424d8fb2070345b369f
Liz <eliz.com>
2019-12-01 10:11:39 +0300

    Complex numbers

---
+++ Srez/Complex.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Srez
+{
+    class Complex
+    {
+        public double active { get; set; }
+        public double passive { get; set; }
+
+        public Complex() { }
+
+        public Complex(double active, double passive)
+        {
+            this.active = active;
+            this.passive = passive;
+        }
+
+        ~Complex()
+        {
+            Console.WriteLine("Deleted");
+        }
+
+        public override string ToString()
+        {
+            return "Active " + this.active + "\n" + "Passive " + this.passive + "\n";
+        }
+
+        public Complex f(int a)
+        {
+            return new Complex(this.active * a, this.passive * a);
+        }
+
+        public Complex abs()
+        {
+            return new Complex(Math.Abs(this.active), Math.Abs(this.passive));
+        }
+
+        public Complex reverse()
+        {
+            double denum = Math.Pow(this.active, 2) + Math.Pow(passive, 2);
+            return new Complex(this.active/denum, -this.passive/denum);
+        }
+    }
+}
